6e27f1bb99c31f9acaab951909e382a17b2c4792
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-04 01:36:20 +0530

    Inserting node(40) in middle

--- src/com/linkedlist/Main.java
+++ src/com/linkedlist/Main.java
@@ -10,62 +10,56 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		/*
-		 * PROCEDURE 1.Adding values to nodes 2. Inserting data to linkedList 3. pop
-		 * data at first node to linkedList 4. Printing linedList nodes 5. pop data at
-		 * lastnode to linkedList 6. Searching node in LL 7. Printing linedList nodes 8.
-		 * Appending data to linkedList
+		 * PROCEDURE 1.Adding values to nodes 2. Inserting data to linkedList 3.
+		 * Printing data after inserting of LL 4. pop data at first node to linkedList
+		 * 5. pop data at lastnode to linkedList 6. Searching node in LL 7. Appending
+		 * data to linkedList
 		 */
 		LL list = new LL();
+
 		/**
 		 *
 		 * 1.Adding values to nodes
 		 */
 		list.add(70);
 		list.add(56);
+		System.out.println("Before data:");
+		list.printLL();
+		System.out.println();
+
 		/**
 		 * 2. Inserting data to linkedList
 		 */
+		list.insert(list.head, 40);
 		list.insert(list.head, 30);
-		System.out.println("Before poping data:");
+
+		/**
+		 * 3. Printing data after inserting of LL
+		 */
+		System.out.println("After insert data:");
 		list.printLL();
 		System.out.println();
 		/**
-		 * 3. pop data at first node to linkedList
+		 * 4. pop data at first node to linkedList
 		 */
 		list.popAtFirst();
-		System.out.println("After poping First data:");
-		/**
-		 * 4. Printing linkedList nodes
-		 */
-		list.printLL();
-		System.out.println();
+
 		/**
 		 * Doing pop at last node
 		 */
-		list.add(56);
-		System.out.println("Before poping data:");
-		list.printLL();
-		System.out.println();
 		/**
 		 * 5. pop data at lastnode to linkedList
 		 */
 		list.popAtLast(70);
-		System.out.println("After poping Last data:");
-		list.printLL();
-		System.out.println();
+
 		/**
 		 * 6. Searching node in LL
 		 */
 		list.search(list.head, 30);
 		list.search(list.head, 70);
-		System.out.println("Data after searting:");
+
 		/**
-		 * 7. Printing linkedList nodes
-		 */
-		list.printLL();
-		System.out.println();
-		/**
-		 * 8. Appending data to linkedList
+		 * 7. Appending data to linkedList
 		 */
 		list.append(56);
 		list.append(30);
