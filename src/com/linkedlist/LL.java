af1f8285c2df8bf69e16bfa2fec4a2818b84b60c
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-03 23:13:43 +0530

    Creating Simple Linked List by taking 70 as head and remaining adding to it

--- src/com/linkedlist/LL.java
+++ src/com/linkedlist/LL.java
@@ -3,29 +3,26 @@ package com.linkedlist;
 public class LL {
 
 	/**
-	 * Simple Linked List Implementation
-	 * created simple linked list
-	 * Assigning value to the nodes
-	 * display nodes
+	 * Simple Linked List Implementation 1.created simple linked list 2.Assigning
+	 * value to the nodes display nodes
 	 * 
 	 */
 	public static void main(String[] args) {
-		
+
 		/**
-		 * 1.created simple linked list
-		 * 2.assigning values to nodes
-		 */ 
-		
-		Node head=new Node(56);
-		Node second=new Node(30);
-		Node third=new Node(70);
-		
-		head.next=second;
-		second.next=third;
-		third.next=null;
+		 * 1.created simple linked list 2.assigning values to nodes
+		 */
+
+		Node head = new Node(70);
+		Node second = new Node(30);
+		Node third = new Node(56);
+
+		head.next = second;
+		second.next = third;
+		third.next = null;
 		/**
 		 * Displaying nodes
 		 */
-		System.out.println(head.data+" "+second.data+" "+third.data);
+		System.out.println(third.data + " " + second.data + " " + head.data);
 	}
 }
--- src/com/linkedlist/Node.java
+++ src/com/linkedlist/Node.java
@@ -3,7 +3,7 @@ package com.linkedlist;
 public class Node {
 	int data;
 	Node next;
-	
+
 	public Node(int data) {
 		this.data = data;
 		this.next = null;
